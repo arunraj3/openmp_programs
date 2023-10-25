d63b1bb971df97c102e280a55dfd03f81f0b3ae9
arunraj3 <arunrajgovindan2002@gmail.com>
2023-10-26 00:39:59 +0530

    matrix_vector_multiplication

--- matrix_vector_multiplication.c
+++ matrix_vector_multiplication.c
@@ -4,5 +4,57 @@
 
 void main()
 {
-    int matrix_size, vector_size;
+    int matrix_row_size, vector_size;
+
+    printf("Enter the size of Matrix : ");
+    scanf("%d", &matrix_row_size);
+
+    printf("Enter the size of Vector : ");
+    scanf("%d", &vector_size);
+
+    if (matrix_row_size != vector_size)
+    {
+        printf("Multiplication us not possible!");
+        exit(0);
+    }
+
+    int matrix[matrix_row_size][matrix_row_size];
+    int vector[vector_size], resultant[matrix_row_size];
+
+#pragma omp parallel
+    {
+        for (int row = 0; row < matrix_row_size; row++)
+        {
+            srand(row); // just to randomise the initial state of generating random numbers
+            vector[row] = rand() % 100;
+            for (int column = 0; column < matrix_row_size; column++)
+            {
+                matrix[row][column] = rand() % 100;
+            }
+        }
+    }
+
+#pragma omp parallel
+    {
+        for (int row = 0; row < matrix_row_size; row++)
+        {
+            resultant[row] = 0;
+            for (int column = 0; column < matrix_row_size; column++)
+            {
+                resultant[row] += matrix[row][column] * vector[row];
+            }
+        }
+    }
+
+    printf("Matrix * Vector = Resultant\n");
+    for (int row = 0; row < matrix_row_size; row++)
+    {
+        printf("[");
+        for (int column = 0; column < matrix_row_size; column++)
+        {
+            printf("%3d ", matrix[row][column]);
+        }
+        printf("]");
+        printf(" * %3d = %6d\n", vector[row], resultant[row]);
+    }
 }
