f7142266e50eb046e48570a894d03ffd00d39393
arunraj3 <arunrajgovindan2002@gmail.com>
2023-10-29 14:15:55 +0530

    Matrix  Matrix multiplication

---
+++ matrix_matrix_multiplication.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+void main()
+{
+    int n;
+    printf("Enter the dimension of square matrices : ");
+    scanf("%d", &n);
+    int i = 0, j = 0, k = 0;
+    int matrix_one[n][n];
+    int matrix_two[n][n];
+    int result_matrix[n][n];
+
+    omp_set_num_threads(64);
+
+#pragma omp parallel private(j)
+    {
+#pragma omp for
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                matrix_one[i][j] = rand() % 100;
+                matrix_two[i][j] = rand() % 100;
+            }
+        }
+    }
+    double start_parallel = omp_get_wtime();
+#pragma omp parallel private(j, k)
+    {
+#pragma omp for
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                result_matrix[i][j] = 0;
+                for (k = 0; k < n; k++)
+                {
+                    result_matrix[i][j] += matrix_one[i][k] * matrix_two[k][j];
+                }
+            }
+        }
+    }
+    double end_parallel = omp_get_wtime();
+    printf("Time taken by parellel Algorithm : %lf\n", (double)(end_parallel - start_parallel));
+    double start = omp_get_wtime();
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            result_matrix[i][j] = 0;
+            for (k = 0; k < n; k++)
+            {
+                result_matrix[i][j] += matrix_one[i][k] * matrix_two[k][j];
+            }
+        }
+    }
+    double end = omp_get_wtime();
+    printf("Time taken by sequential Algorithm : %lf\n", (double)(end - start));
+}
